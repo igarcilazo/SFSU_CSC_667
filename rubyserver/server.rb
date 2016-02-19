64a984ea61a2a5c758ff008976fcf0b33edfd565
Izaac Garcilazo <izaacgarcilazo@msn.com>
2016-02-18 20:50:30 -0800

    Added files via upload

--- rubyserver/server.rb
+++ rubyserver/server.rb
@@ -1,30 +1,35 @@
-require 'socket' # allows use of TCPServer & TCPSocket classes
+require 'socket' # allows use of TCPServer & TCPSocket classesH
 
-DEFAULT_PORT = 56790
+#DEFAULT_PORT = 56792
 
 class WebServer
   attr_reader :options, :socket
   
   def initialize(options={})
     @options = options
+    #Open webserver configuration and mime types
+    @httpd = HttpdConf.new(File.open("config/httpd.conf", "r").read())
+    #@mimefile = File.open("config/mime.types", "r")
   end
   
   def start
+    @portnumber = @httpd.port
     loop do
       puts "Opening server socket to listen for connections"
       @socket = server.accept # open socket, wait until client connects
-      
+      Thread.new(@socket) do |newsocket|#Thread for every session
       puts "Received connection"
-      Request.new(@socket).parse
-      @socket.puts Response.new.to_s
+      Request.new(newsocket).parse
+      newsocket.puts Response.new.to_s
       
-      @socket.close # terminate connection
+      newsocket.close # terminate connection
     end
+  end
   end
   
   # TCPServer represents a TCP/IP server socket
   def server
-    @server ||= TCPServer.open(options.fetch('localhost', DEFAULT_PORT))
+    @server ||= TCPServer.open(options.fetch('localhost', @portnumber))
   end
 end
 
@@ -82,5 +87,37 @@ class Response # generates generic OK response to send to the client
            "#{@body}\r\n"
   end
 end
+
+class HttpdConf 
+    def initialize(httpdConfig)
+      @file = httpdConfig.split("\n")
+      @httpdhash = {}
+    end
+    def root
+      @httpdhash[:root] = find("ServerRoot")
+    end
+    def docRoot
+      @httpdhash[:docRoot] = find("DocumentRoot")
+    end
+    def port
+      @httpdhash[:port] = find("port").to_i
+    end
+    def log
+      @httpdhash[:log] = find("LogFile")
+    end
+    def errorlog
+      @httpdhash[:errorlog] = find("ErrorLogFile")
+    end
+    def find(resource)
+      value = ""
+      @file.each do |line|
+        if line.include? resource
+          value = line.split(" ")
+          break
+        end
+      end
+      value = value[1]
+    end
+  end
 
 WebServer.new.start
